6f37b24b2d8a443c3be7f8f8f141a42440770c34
meilihao <563278383@qq.com>
2017-02-09 17:56:16 +0800

    .

--- serial.go
+++ serial.go
@@ -1,5 +1,15 @@
 package water
 
+import (
+	"github.com/satori/go.uuid"
+)
+
 type SerialAdapter interface {
 	Id() string
+}
+
+type DefaultSerial struct{}
+
+func (s DefaultSerial) Id() string {
+	return uuid.NewV1().String()
 }
