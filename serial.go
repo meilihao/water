3523125e678e71ab3397c7b090507e6aaa0ae1ec
meilihao <563278383@qq.com>
2018-05-14 14:42:53 +0800

    m uuid的变化

--- serial.go
+++ serial.go
@@ -13,5 +13,5 @@ type SerialAdapter interface {
 type DefaultSerial struct{}
 
 func (s DefaultSerial) Id() string {
-	return strings.Replace(uuid.NewV1().String(), "-", "", -1)
+	return strings.Replace(uuid.Must(uuid.NewV1()).String(), "-", "", -1)
 }
