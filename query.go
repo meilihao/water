63829c510d6ca6b0fc657effca51611ba32865c5
meilihao <563278383@qq.com>
2021-04-19 13:22:45 +0800

    r QueryString -> Query

--- query.go
+++ query.go
@@ -26,10 +26,10 @@ func (ctx *Context) parseFormOrMultipartForm() error {
 	return nil
 }
 
-// QueryString returns escapred and trimmed string.
+// Query returns escapred and trimmed string.
 // Note: It is recommended! If not, you can use "ctx.Request.FormValue".
 // 这是推荐的做法,如果不认同,可使用ctx.Request.FormValue.
-func (ctx *Context) QueryString(name string) string {
+func (ctx *Context) Query(name string) string {
 	if err := ctx.parseFormOrMultipartForm(); err != nil {
 		panic(err.Error())
 	}
@@ -38,42 +38,42 @@ func (ctx *Context) QueryString(name string) string {
 
 // QueryBool returns bool
 func (ctx *Context) QueryBool(name string) bool {
-	v, _ := strconv.ParseBool(ctx.QueryString(name))
+	v, _ := strconv.ParseBool(ctx.Query(name))
 	return v
 }
 
 // QueryInt returns int
 func (ctx *Context) QueryInt(name string) int {
-	v, _ := strconv.Atoi(ctx.QueryString(name))
+	v, _ := strconv.Atoi(ctx.Query(name))
 	return v
 }
 
 // QueryInt returns int64
 func (ctx *Context) QueryInt64(name string) int64 {
-	v, _ := strconv.ParseInt(ctx.QueryString(name), 10, 64)
+	v, _ := strconv.ParseInt(ctx.Query(name), 10, 64)
 	return v
 }
 
 // QueryUint returns uint
 func (ctx *Context) QueryUint(name string) uint {
-	v, _ := strconv.ParseUint(ctx.QueryString(name), 10, 64)
+	v, _ := strconv.ParseUint(ctx.Query(name), 10, 64)
 	return uint(v)
 }
 
 // QueryUint64 returns uint64
 func (ctx *Context) QueryUint64(name string) uint64 {
-	v, _ := strconv.ParseUint(ctx.QueryString(name), 10, 64)
+	v, _ := strconv.ParseUint(ctx.Query(name), 10, 64)
 	return v
 }
 
 // QueryFloat64 returns float64
 func (ctx *Context) QueryFloat64(name string) float64 {
-	v, _ := strconv.ParseFloat(ctx.QueryString(name), 64)
+	v, _ := strconv.ParseFloat(ctx.Query(name), 64)
 	return v
 }
 
-// QueryStrings returns a list of results by given query name
-func (ctx *Context) QueryStrings(name string) []string {
+// Querys returns a list of results by given query name
+func (ctx *Context) Querys(name string) []string {
 	if err := ctx.parseFormOrMultipartForm(); err != nil {
 		panic(err.Error())
 	}
