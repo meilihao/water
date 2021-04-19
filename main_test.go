ee3d4739eaa1f3c2301da63456b26d4c56829e6f
meilihao <563278383@qq.com>
2021-04-19 19:31:11 +0800

    + main_test

---
+++ main_test.go
@@ -0,0 +1,71 @@
+package water
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	router := NewRouter()
+
+	router.Use(middleware)
+
+	router.GET("/", test)
+	router.GET("/help", test)
+	router.ANY("/about", test)
+	router.HEAD("/about", test)
+	router.OPTIONS("/*")
+
+	router.Group("/a", func(r *Router) {
+		r.Use(middleware)
+
+		r.GET("/1", test)
+		r.GET("/<id:int>", test2)
+		r.Group("/b", func(r *Router) {
+			r.GET("", test, test)
+			r.ANY("/2", test)
+			r.PUT("/<id ~ 70|80>", test2)
+			r.GET("/*", test)
+		})
+	})
+	router.GET("/d2/<id ~ z(d*)b>", test3)
+	router.GET("/d2/<id1+id2 ~ z(d*)h(u)b>", test3)
+
+	w := router.Handler()
+
+	fmt.Println("\n\n", "Raw Router Tree:")
+	w.PrintRawRouter()
+
+	fmt.Println("\n\n", "GET's Routes:")
+	w.PrintRawRoutes("GET")
+
+	fmt.Println("\n\n", "All Routes:")
+	w.PrintRawAllRoutes()
+
+	fmt.Println("\n\n", "GET's Release Router Tree:")
+	w.PrintRouterTree("GET")
+
+	// if err := w.ListenAndServe(":8081"); err != nil {
+	// 	log.Fatalln(err)
+	// }
+}
+
+func middleware(ctx *Context) {
+	fmt.Println("1")
+
+	ctx.Next()
+
+	fmt.Println("2")
+}
+
+func test(ctx *Context) {
+	ctx.String(200, ctx.Request.RequestURI)
+}
+
+func test2(ctx *Context) {
+	ctx.JSON(200, ctx.Params.MustInt("id"))
+}
+
+func test3(ctx *Context) {
+	ctx.JSON(200, ctx.Params)
+}
