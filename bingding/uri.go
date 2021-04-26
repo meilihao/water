12126b9ca2b5e3dc85b367d63a69f7fac8208a0f
meilihao <563278383@qq.com>
2021-04-26 20:45:06 +0800

    .

--- bingding/uri.go
+++ bingding/uri.go
@@ -6,14 +6,14 @@ func (uriBinding) Name() string {
 	return "uri"
 }
 
-func (uriBinding) Bind(m map[string][]string, obj interface{}) error {
+func (uriBinding) Bind2(m map[string][]string, obj interface{}) error {
 	if err := mapFormByTag(obj, m, "uri"); err != nil {
 		return err
 	}
 	return validate(obj)
 }
 
-func (uriBinding) Bind2(m map[string][]string, obj interface{}) error {
+func (uriBinding) Bind(m map[string][]string, obj interface{}) error {
 	if err := MapForm(obj, m, nil, "uri"); err != nil {
 		return err
 	}
