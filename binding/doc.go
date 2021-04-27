9674f10aabf41cf0071b986845433fb067eec1ac
meilihao <563278383@qq.com>
2021-04-27 13:11:16 +0800

    u binding doc

--- binding/doc.go
+++ binding/doc.go
@@ -1,4 +1,4 @@
 package binding
 
 // core code from https://github.com/gin-gonic/gin/binding and github.com/go-macaron/binding
-// xxx.Bind2() is from gin
+// xxx.Bind2() is from gin. xxx.Bind() is from macaron + myself
